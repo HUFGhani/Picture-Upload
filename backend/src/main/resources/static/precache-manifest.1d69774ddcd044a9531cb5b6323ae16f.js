95a1973baf2d2369f57ee2523d8e94c7aea66cd6
Hamza Umar Farooq Ghani <hamzaumarfarooqghani@rentalcars.com>
2019-02-22 16:08:59 +0000

    start point

---
+++ backend/src/main/resources/static/precache-manifest.1d69774ddcd044a9531cb5b6323ae16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dd58a3c056b772cab904",
+    "url": "/static/js/main.dd58a3c0.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "dd58a3c056b772cab904",
+    "url": "/static/css/main.05adc99d.chunk.css"
+  },
+  {
+    "revision": "cc8ce69f4a3c3d2608b8131755ee44e8",
+    "url": "/index.html"
+  }
+];
---
+++ backend/src/test/kotlin/io/github/hufghani/PictureUploader/PictureUploaderApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.github.hufghani.PictureUploader
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class PictureUploaderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
